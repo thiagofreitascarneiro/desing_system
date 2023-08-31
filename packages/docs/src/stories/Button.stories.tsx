697907c3c30b63d4ee84be4ac29834b30c6b7bc3
thiagofreitascarneiro <thiagofreitascarneiro@hotmail.com>
2023-08-31 17:24:52 -0300

    feat: configurando projeto

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,15 @@
+import { StoryObj, Meta } from "@storybook/react"
+import  {Button} from "@ignite-ui/react"
+
+export default {
+    title: 'Button',
+    Component: Button,
+} as Meta
+
+export const Primary: StoryObj = {
+
+}
+
+export const Secondary: StoryObj = {
+
+}
