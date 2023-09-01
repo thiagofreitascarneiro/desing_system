97b1077a97cdfd9434116b6163c4c581b80f9835
thiagofreitascarneiro <thiagofreitascarneiro@hotmail.com>
2023-09-01 13:22:53 -0300

    feat: criando storie book Button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,15 +1,21 @@
 import { StoryObj, Meta } from "@storybook/react"
-import  {Button} from "@ignite-ui/react"
+import  {Button, ButtonProps } from "@ignite-ui/react"
 
 export default {
     title: 'Button',
-    Component: Button,
-} as Meta
+    component: Button,
 
-export const Primary: StoryObj = {
+    args: {
+        children: 'Enviar',
+    }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
 
 }
 
-export const Secondary: StoryObj = {
-
+export const Big: StoryObj<ButtonProps> = {
+    args: {
+        size: 'big'
+    }
 }
