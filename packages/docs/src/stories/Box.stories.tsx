21b521be005a41c3dc2de08b12f7676a646e01db
thiagofreitascarneiro <thiagofreitascarneiro@hotmail.com>
2023-09-04 11:15:27 -0300

    feat: criadno component box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,18 @@
+import { StoryObj, Meta } from "@storybook/react"
+import  { Box, BoxProps } from "@ignite-ui/react"
+
+export default {
+    title: 'Surfaces/Box',
+    component: Box,
+    args: {
+        children: (
+           <>
+            <span>Testando o elemento Box</span>
+           </>
+        ),
+    },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {
+
+}
